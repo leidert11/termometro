eed7fc3407dbb79373f34b075023f1c9ac8eab51
fanart13 <110786757+leidert11@users.noreply.github.com>
2022-09-26 00:13:04 -0500

    Update termometro.js

--- termometro.js
+++ termometro.js
@@ -1,9 +1,7 @@
 let convertidorf=function () {
     let value=document.getElementById('number').value;
     let calcular = ((value-32)*5/9).toFixed(1)
-    // if (celsius>0){
-    //     temperatura.style.heigth = "" + celsius
-    // }
+
     let llenar = 300/100*calcular
     document.getElementById("grados").value= calcular
     document.getElementById('mercurio').style.height=llenar+'px'
